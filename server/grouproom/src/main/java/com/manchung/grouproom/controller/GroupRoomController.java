28c06902f396e4937f5549264006744025db354e
Hyunggeun Kim <hgk0000@naver.com>
2025-03-26 12:52:04 +0900

    feat : swagger error code update

--- server/grouproom/src/main/java/com/manchung/grouproom/controller/GroupRoomController.java
+++ server/grouproom/src/main/java/com/manchung/grouproom/controller/GroupRoomController.java
@@ -28,7 +28,15 @@ public class GroupRoomController {
 
     @Operation(
             summary = "액세스 토큰 조회",
-            description = "리프레시 토큰을 이용하여 액세스 토큰을 조회합니다"
+            description =
+            """
+            리프레시 토큰을 이용하여 액세스 토큰을 조회합니다\n
+            에러코드\n
+            code : 10001 -> Access Token이 만료되었습니다.\n
+            code : 10002 -> 잘못된 Access Token입니다.\n
+            code : 10003 -> Refresh Token이 존재하지 않습니다.\n
+            code : 10004 -> Refresh Token이 만료되었습니다.\n
+            """
     )
     @PostMapping("/refresh-token")
     public RefreshTokenResponse refreshAccessToken(
@@ -39,7 +47,12 @@ public class GroupRoomController {
 
     @Operation(
             summary = "회원가입",
-            description = "사용자를 등록하고 이메일 및 비밀번호를 저장합니다."
+            description =
+            """
+            사용자를 등록하고 이메일 및 비밀번호를 저장합니다\n
+            에러코드\n
+            code : 20004 -> 입력하신 정보가 올바르지 않습니다\n
+            """
     )
     @PostMapping("/signUp")
     public SignUpResponse signUp(@RequestBody SignUpRequest request) {
@@ -48,7 +61,13 @@ public class GroupRoomController {
 
     @Operation(
             summary = "로그인",
-            description = "이메일과 비밀번호로 로그인하여 JWT 토큰을 반환합니다."
+            description =
+            """
+            이메일과 비밀번호로 로그인하여 JWT 토큰을 반환합니다\n
+            에러코드\n
+            code : 20002 -> 해당 이메일의 사용자가 없습니다\n
+            code : 20003 -> 비밀번호가 일치하지 않습니다\n
+            """
     )
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
@@ -57,7 +76,16 @@ public class GroupRoomController {
 
     @Operation(
             summary = "방 예약 요청",
-            description = "사용자가 특정 룸을 예약하는 기능입니다."
+            description =
+            """
+            사용자가 특정 룸을 예약하는 기능입니다\n
+            에러코드\n
+            code : 20001 -> 사용자를 찾을 수 없습니다\n
+            code : 30001 -> 해당 소그룹실을 찾을 수 없습니다\n
+            code : 30004 -> 신청은 월요일에만 가능합니다\n
+            code : 30005 -> 신청 가능 시간이 아닙니다. (월요일 00:00~21:00)\n
+            code : 30006 -> 이미 신청했거나 신청할 수 없는 상태입니다\n
+            """
     )
     @PostMapping("/reserveRoom")
     public RoomReservationResponse reserve(
@@ -68,7 +96,14 @@ public class GroupRoomController {
 
     @Operation(
             summary = "예약 가능 여부 확인",
-            description = "해당 사용자가 특정 룸을 예약할 수 있는지 확인합니다."
+            description =
+            """
+            해당 사용자가 특정 룸을 예약할 수 있는지 확인합니다\n
+            에러코드\n
+            code : 30004 -> 신청은 월요일에만 가능합니다\n
+            code : 30005 -> 신청 가능 시간이 아닙니다. (월요일 00:00~21:00)\n
+            code : 30006 -> 이미 신청했거나 신청할 수 없는 상태입니다\n
+            """
     )
     @GetMapping("/checkReservationApplicable")
     public CheckReservationApplicableResponse checkReservation(
@@ -78,7 +113,12 @@ public class GroupRoomController {
 
     @Operation(
             summary = "방과 예약 정보 조회",
-            description = "모든 방에 대한 모든 예약 정보를 가져옵니다."
+            description =
+            """
+            모든 방에 대한 모든 예약 정보를 가져옵니다\n
+            에러코드\n
+            없음\n
+            """
     )
     @GetMapping("/getRoomWithReservationInfo")
     public List<RoomWithReservationInfoResponse> getRoomInfo(
@@ -88,7 +128,12 @@ public class GroupRoomController {
 
     @Operation(
             summary = "사용자 이용 상태 조회",
-            description = "사용자의 이용 상태를 조회합니다."
+            description =
+            """
+            사용자의 이용 상태를 조회합니다\n
+            에러코드\n
+            code : 20001 -> 사용자를 찾을 수 없습니다\n
+            """
     )
     @GetMapping("/getUserUsageStatus")
     public UserUsageStatusResponse usageStatus(
--- server/grouproom/src/main/java/com/manchung/grouproom/error/ErrorCode.java
+++ server/grouproom/src/main/java/com/manchung/grouproom/error/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     // ✅ 인증 관련 오류
     ACCESS_TOKEN_EXPIRED(10001, HttpStatus.UNAUTHORIZED, "Access Token이 만료되었습니다."),
-    INVALID_REFRESH_TOKEN(10002, HttpStatus.UNAUTHORIZED, "Refresh Token이 유효하지 않습니다."),
+    INVALID_REFRESH_TOKEN(10002, HttpStatus.UNAUTHORIZED, "잘못된 Access Token입니다."),
     REFRESH_TOKEN_NOT_FOUND(10003, HttpStatus.UNAUTHORIZED, "Refresh Token이 존재하지 않습니다."),
     REFRESH_TOKEN_EXPIRED(10004, HttpStatus.UNAUTHORIZED, "Refresh Token이 만료되었습니다."),
 
@@ -17,11 +17,11 @@ public enum ErrorCode {
     USER_NOT_FOUND(20001, HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     INVALID_EMAIL(20002, HttpStatus.CONFLICT, "해당 이메일의 사용자가 없습니다."),
     PASSWORD_MISMATCH(20003, HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
-    SIGNUP_WRONG_INFORMATION(20003, HttpStatus.UNAUTHORIZED, "입력하신 정보가 올바르지 않습니다."),
+    SIGNUP_WRONG_INFORMATION(20004, HttpStatus.UNAUTHORIZED, "입력하신 정보가 올바르지 않습니다."),
 
     // ✅ 예약 관련 오류
-    ROOM_NOT_FOUND(30001, HttpStatus.NOT_FOUND, "해당 룸을 찾을 수 없습니다."),
-    ROOM_ALREADY_RESERVED(30002, HttpStatus.CONFLICT, "이미 예약된 룸입니다."),
+    ROOM_NOT_FOUND(30001, HttpStatus.NOT_FOUND, "해당 소그룹실을 찾을 수 없습니다."),
+    ROOM_ALREADY_RESERVED(30002, HttpStatus.CONFLICT, "이미 예약된 소그룹실입니다."),
     RESERVATION_NOT_ALLOWED(30003, HttpStatus.FORBIDDEN, "예약할 수 없는 사용자입니다."),
     RESERVATION_ONLY_ALLOWED_MONDAY(30004, HttpStatus.FORBIDDEN, "신청은 월요일에만 가능합니다."),
     RESERVATION_NOT_ALLOWED_TIME(30005, HttpStatus.FORBIDDEN, "신청 가능 시간이 아닙니다. (월요일 00:00~21:00)"),
